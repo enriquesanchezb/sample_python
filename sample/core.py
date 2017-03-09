27a68662ef5c0c780ac51a9d8da544ced4f88e82
Enrique Sanchez <enrique.sanchez@cabify.com>
2017-03-09 10:28:39 +0100

    Added tests

--- sample/core.py
+++ sample/core.py
@@ -2,6 +2,14 @@
 
 def add(a, b):
     """Add two numbers"""
+
+    if((type(a) is bool) and (type(b) is bool)):
+        return a or b
+    if((type(a) is bool) and not(type(b) is bool)):
+        raise TypeError
+    if ((type(b) is str) or (type(a) is str)):
+        a = str(a)
+        b = str(b)
     return a + b
 
 if __name__ == "__main__":
--- tests/test_core.py
+++ tests/test_core.py
@@ -9,10 +9,42 @@ class CoreTestSuite(unittest.TestCase):
     """Test cases for Core"""
 
     def test_add(self):
-        self.assertEqual(sample.add(1,1),2)
+        # SET-UP
+        num1 = 1
+        num2 = 2
+
+        # EXECUTION
+        result = sample.add(num1,num2)
+
+        # ASSERT
+        self.assertEqual(result, 3, "Los números no son iguales")
 
     def test_add_not_invalid_result(self):
-        self.assertNotEqual(sample.add(1,1),3)
+        self.assertNotEqual(sample.add(1,2),3)
+
+
+    def test_add_int_string(self):
+        self.assertEqual(sample.add(1,'patata'),'1patata')
+
+    def test_add_string_int(self):
+        self.assertEqual(sample.add('patata', 1),'patata1')
+
+    def test_add_int_float(self):
+        self.assertEqual(sample.add(1, 2.1), 3.1)
+
+    def test_add_float_int(self):
+        self.assertEqual(sample.add(2.1, 1), 3.1)
+
+    def test_add_bool_string(self):
+        self.assertRaises(sample.add(True, 'hola'), TypeError)
+
+    def test_add_string_bool(self):
+        self.assertRaises(sample.add('hola', True), TypeError)
+
+    def test_add_bool_bool(self):
+        self.assertEqual(sample.add(True, True), True)
+
+
 
 if __name__ == '__main__':
     unittest.main()
